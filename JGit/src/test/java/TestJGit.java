2e905824bee65950651fde6aee7a5166613cf349
fansq-j <13552796829@163.com>
2019-11-07 16:27:21 +0800

    版本回滚

--- JGit/src/test/java/TestJGit.java
+++ JGit/src/test/java/TestJGit.java
@@ -3,7 +3,9 @@ import java.io.File;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.Callable;
@@ -12,6 +14,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
 
 import org.eclipse.jgit.api.CheckoutCommand;
+import org.eclipse.jgit.api.CommitCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.MergeCommand;
 import org.eclipse.jgit.api.MergeResult;
@@ -19,7 +22,11 @@ import org.eclipse.jgit.api.Status;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.NoHeadException;
 import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.diff.DiffEntry.ChangeType;
 import org.eclipse.jgit.diff.DiffFormatter;
+import org.eclipse.jgit.diff.Edit;
+import org.eclipse.jgit.diff.EditList;
+import org.eclipse.jgit.diff.RenameDetector;
 import org.eclipse.jgit.errors.IncorrectObjectTypeException;
 import org.eclipse.jgit.errors.MissingObjectException;
 import org.eclipse.jgit.errors.StopWalkException;
@@ -30,6 +37,8 @@ import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.PersonIdent;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.patch.FileHeader;
+import org.eclipse.jgit.patch.HunkHeader;
 //import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevTree;
@@ -161,6 +170,7 @@ public class TestJGit {
 			results.forEach(commit -> {
 				PersonIdent authoIdent = commit.getAuthorIdent();
 				System.out.println("提交人：  " + authoIdent.getName() + "     <" + authoIdent.getEmailAddress() + ">");
+				System.out.println("版本号： "+commit.getName());
 				System.out.println("提交SHA1：  " + commit.getId().name());
 				System.out.println("提交信息：  " + commit.getShortMessage());
 				System.out.println("提交时间：  " + format.format(authoIdent.getWhen()));
@@ -284,7 +294,7 @@ public class TestJGit {
 	/**
 	 * 合并分支
 	 */
-	@Test
+	//@Test
 	public void mergeBranch() {
 		  Git git;
 		try {
@@ -314,7 +324,7 @@ public class TestJGit {
 	 * 返回指定分支 指定文件的内容 ，可以做成循环 然后比较同一个文件在
 	 * 不同分支的差异
 	 */
-	@Test
+	//@Test
     public void  getContentWithFile(){
     	String gitRoot ="D:\\\\source-git\\\\.git";
     	String branchName="TestJGit";
@@ -365,11 +375,13 @@ public class TestJGit {
 	
 	/**
 	 * 比较两个版本差异
-	 * @param Child
-	 * @param Parent
+	 * @param Child 8afe249dce75397afc57fc5bda9f4efcda5da1b0
+	 * @param Parent 8f031ae5c8fed015ca6af3675f9b43cf0466a3ab
 	 */
 	@Test
-	public void diffMethod(String Child, String Parent){
+	public void diffMethod(){
+		String Child ="8afe249dce75397afc57fc5bda9f4efcda5da1b0" ;
+		String Parent = "8f031ae5c8fed015ca6af3675f9b43cf0466a3ab";
 		Git git;
 		try {
 			git=Git.open(new File("D:\\\\source-git\\\\.git"));
@@ -392,11 +404,39 @@ public class TestJGit {
 				    df.setRepository(git.getRepository());
 				
 				for (DiffEntry diffEntry : diffs) {
-			         df.format(diffEntry);
-			         String diffText = out.toString("UTF-8");
-			         System.out.println(diffText);
+					System.out.println(diffEntry);
+					df.format(diffEntry);
+					//System.out.println(out.toString());
+					FileHeader file = df.toFileHeader(diffEntry);
+//					List<HunkHeader> hunks = (List<HunkHeader>) file.getHunks();
+//	                int addSize = 0;
+//	                int subSize = 0;
+//	                for(HunkHeader hunkHeader:hunks){
+//	                	EditList editList = hunkHeader.toEditList();
+//	                	for(Edit edit : editList){
+//	                		edit.
+////	                		subSize += edit.getEndA()-edit.getBeginA();
+////	                		addSize += edit.getEndB()-edit.getBeginB();
+//	                		
+//	                	}
+//	                }
+					
+					
+					
+//			         System.out.println("差异 ： "+file.getScriptText());
+//			         System.out.println("buffer: "+new String(file.getBuffer()));
+//			         System.out.println("score: "+file.getScore());
+//			         System.out.println("att: "+file.getDiffAttribute());
+//			         System.out.println("change: "+file.getChangeType());
+//			         System.out.println("end: "+file.getEndOffset());
+//			         System.out.println("newid: "+file.getNewId());
+//			         System.out.println("newmo: "+file.getNewMode());
+//			         System.out.println("old: "+file.getOldPath());
+//			         String diffText = out.toString("UTF-8");
+			         //System.out.println(diffText);
 			       //  out.reset();
 				}
+				//System.out.println("收拾收拾2" + out.toString());
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
@@ -409,5 +449,116 @@ public class TestJGit {
 			e.printStackTrace();
 		}
 	}
+
+	/**
+     * 回滚到指定版本的上一个版本
+     * @param gitRoot git仓库目录
+     * @param diffEntries 需要回滚的文件
+     * @param revision 版本号
+     * @param remark 备注
+     * @return
+     * @throws Exception
+     */
+    public static boolean rollBackPreRevision(String gitRoot, List<DiffEntry> diffEntries,
+                                              String revision, String remark) throws Exception {
+ 
+        if (diffEntries == null || diffEntries.size() == 0) {
+            throw new Exception("没有需要回滚的文件");
+        }
+ 
+        Git git = Git.open(new File(gitRoot));
+ 
+        List<String> files = new ArrayList<String>();
+ 
+        //注意：下面的reset命令会将暂存区的内容恢复到指定（revesion）的状态，相当于取消add命令的操作
+        /*Repository repository = git.getRepository();
+        RevWalk walk = new RevWalk(repository);
+        ObjectId objId = repository.resolve(revision);
+        RevCommit revCommit = walk.parseCommit(objId);
+        String preVision = revCommit.getParent(0).getName();
+        ResetCommand resetCmd = git.reset();
+        for (String file : files) {
+            resetCmd.addPath(file);
+        }
+        resetCmd.setRef(preVision).call();
+        repository.close();*/
+ 
+        //取出需要回滚的文件，新增的文件不回滚
+        for (DiffEntry diffEntry : diffEntries) {
+            if (diffEntry.getChangeType() == ChangeType.DELETE) {
+                continue;
+            } else {
+                files.add(diffEntry.getNewPath());
+            }
+        }
+ 
+        if (files.size() == 0) {
+            throw new Exception("没有需要回滚的文件");
+        }
+ 
+        //checkout操作会丢失工作区的数据，暂存区和工作区的数据会恢复到指定（revision）的版本内容
+        CheckoutCommand checkoutCmd = git.checkout();
+        for (String file : files) {
+            checkoutCmd.addPath(file);
+        }
+        //加了“^”表示指定版本的前一个版本，如果没有上一版本，在命令行中会报错，例如：error: pathspec '4.vm' did not match any file(s) known to git.
+        checkoutCmd.setStartPoint(revision + "^");
+        checkoutCmd.call();
+ 
+        //重新提交一次
+        CommitCommand commitCmd = git.commit();
+        for (String file : files) {
+            commitCmd.setOnly(file);
+        }
+        commitCmd.setCommitter("yonge", "654166020@qq.com").setMessage(remark).call();
+ 
+        return true;
+    }
+ 
+    /**
+     * 获取上一版本的变更记录，如果是新增的文件，不会显示，因为做回滚时不需要回滚新增的文件
+     * @param gitRoot git仓库目录
+     * @param revision 版本号
+     * @return
+     * @throws Exception
+     */
+    public static List<DiffEntry> rollBackFile(String gitRoot, String revision) throws Exception {
+ 
+        Git git = Git.open(new File(gitRoot));
+        Repository repository = git.getRepository();
+ 
+        ObjectId objId = repository.resolve(revision);
+        Iterable<RevCommit> allCommitsLater = git.log().add(objId).call();
+        Iterator<RevCommit> iter = allCommitsLater.iterator();
+        RevCommit commit = iter.next();
+        TreeWalk tw = new TreeWalk(repository);
+        tw.addTree(commit.getTree());
+        commit = iter.next();
+        if (commit != null) {
+            tw.addTree(commit.getTree());
+        } else {
+            throw new Exception("当前库只有一个版本，不能获取变更记录");
+        }
+ 
+        tw.setRecursive(true);
+        RenameDetector rd = new RenameDetector(repository);
+        rd.addAll(DiffEntry.scan(tw));
+        List<DiffEntry> diffEntries = rd.compute();
+        if (diffEntries == null || diffEntries.size() == 0) {
+            return diffEntries;
+        }
+        Iterator<DiffEntry> iterator = new ArrayList<DiffEntry>(diffEntries).iterator();
+        DiffEntry diffEntry = null;
+        while (iterator.hasNext()) {
+            diffEntry = iterator.next();
+            System.out.println("newPath:" + diffEntry.getNewPath() + "    oldPath:"
+                               + diffEntry.getOldPath() + "   changeType:"
+                               + diffEntry.getChangeType());
+            if (diffEntry.getChangeType() == ChangeType.DELETE) {
+                iterator.remove();
+            }
+        }
+        return diffEntries;
+    }
 
 }
